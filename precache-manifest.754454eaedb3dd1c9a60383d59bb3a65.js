f9145c78dd80e564db8f59deb30e0d41e087141f
Amund Bjoernstad <amund.bjornstad@outlook.com>
2019-12-03 12:29:25 +0100

    Updates

---
+++ precache-manifest.754454eaedb3dd1c9a60383d59bb3a65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0301401824f4fff8ee80d73d11e487",
+    "url": "/temaoppgave-post-it/index.html"
+  },
+  {
+    "revision": "c6f0a7a94d1e9c46c99d",
+    "url": "/temaoppgave-post-it/static/css/main.900150fb.chunk.css"
+  },
+  {
+    "revision": "3271056a85abfe765634",
+    "url": "/temaoppgave-post-it/static/js/2.2fb7ba0b.chunk.js"
+  },
+  {
+    "revision": "c6f0a7a94d1e9c46c99d",
+    "url": "/temaoppgave-post-it/static/js/main.230fd26a.chunk.js"
+  },
+  {
+    "revision": "e8be28fa6eab5fef464f",
+    "url": "/temaoppgave-post-it/static/js/runtime-main.bba95346.js"
+  }
+]);
